aabcba2d5e6efd54991e3c3aeca99e147078cdd6
ootori <webmaster@ekaki3.net>
2016-04-28 08:51:23 +0900

    コードの整形

--- src/AuLait/View.php
+++ src/AuLait/View.php
@@ -1,71 +1,70 @@
 <?php
-namespace AuLait {
+namespace AuLait;
 
-    class View
+class View
+{
+    protected $path = '';
+    protected $data = [];
+    protected $layout = null;
+
+    /**
+     * @param array $data
+     */
+    public function __construct($data = [])
     {
-        protected $path = '';
-        protected $data = [];
-        protected $layout = null;
-
-        /**
-         * @param array $data
-         */
-        public function __construct($data = [])
-        {
-            $this->data = $data;
-        }
-
-        /**
-         * @param string $path
-         */
-        public function setPath($path)
-        {
-            $this->path = $path;
-        }
-
-        /**
-         * @param string $file
-         */
-        public function setLayout($file)
-        {
-            $this->layout = $file;
-        }
-
-        /**
-         * @param string $key
-         * @param mixed $value
-         */
-        public function assign($key, $value)
-        {
-            $this->data[$key] = $value;
-        }
-
-        /**
-         * @param $file
-         * @return string
-         */
-        public function render($file)
-        {
-            set_include_path($this->path);
-            extract((array)$this->data);
-            ob_start();
-            require $this->path . '/' . $file . '.php';
-            $body = ob_get_contents();
-            ob_end_clean();
-            if ($this->layout) {
-                require $this->path . '/' . $this->layout . '.php';
-            } else {
-                return $body;
-            }
-        }
-
-        /**
-         * @param $file
-         */
-        public function display($file)
-        {
-            echo $this->render($file);
-        }
-
+        $this->data = $data;
     }
+
+    /**
+     * @param string $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @param string $file
+     */
+    public function setLayout($file)
+    {
+        $this->layout = $file;
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function assign($key, $value)
+    {
+        $this->data[$key] = $value;
+    }
+
+    /**
+     * @param $file
+     * @return string
+     */
+    public function render($file)
+    {
+        set_include_path($this->path);
+        extract((array)$this->data);
+        ob_start();
+        require $this->path . '/' . $file . '.php';
+        $body = ob_get_contents();
+        ob_end_clean();
+        if ($this->layout) {
+            require $this->path . '/' . $this->layout . '.php';
+        } else {
+            return $body;
+        }
+    }
+
+    /**
+     * @param $file
+     */
+    public function display($file)
+    {
+        echo $this->render($file);
+    }
+
 }
