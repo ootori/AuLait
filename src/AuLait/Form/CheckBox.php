39223bff7906cb167b50fe275cb3f164503cfe8b
ootori <webmaster@ekaki3.net>
2019-01-20 15:23:32 +0900

    不要な行の削除

--- src/AuLait/Form/CheckBox.php
+++ src/AuLait/Form/CheckBox.php
@@ -4,6 +4,7 @@ namespace AuLait\Form;
 class CheckBox extends Element
 {
     protected $form_type = 'checkbox';
+//    protected $form_value = '1';
 
     public function render($params = [])
     {
